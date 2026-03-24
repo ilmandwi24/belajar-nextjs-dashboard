f527b76ff34b26386c6a8ebd858b4617a9d8b547
ilmandwi24 <ilmandwi20@gmail.com>
2026-03-24 12:12:12 +0700

    fix delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -129,17 +129,17 @@ export async function updateInvoice(  id: string,
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
+    // await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
-//   try {
-//     await sql`DELETE FROM invoices WHERE id = ${id}`;
-//   } catch (error) {
-//     // We'll also log the error to the console for now
-//     console.error(error);
-//     return {
-//       message: "Database Error: Failed to Create Invoice.",
-//     };
-//   }
+  } catch (error) {
+    // We'll also log the error to the console for now
+    console.error(error);
+    return {
+      message: "Database Error: Failed to Create Invoice.",
+    };
+  }
   revalidatePath("/dashboard/invoices");
 }
 
